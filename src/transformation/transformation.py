a54f278edc6035b67a1a4c27732f43392cfc5925
Satyam Prajapati <sp.17cs33@thdcihet.ac.in>
2026-07-29 16:27:23 +0530

    Add reusable dirty data transformation module

---
+++ src/transformation/transformation.py
@@ -0,0 +1,70 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import when, col, lit
+
+def introduce_nulls(df: DataFrame, column_name: str, condition):
+
+    return df.withColumn(
+        column_name,
+        when(condition, lit(None))
+        .otherwise(col(column_name))
+    )
+
+def introduce_duplicates(df, number_of_rows):
+    duplicate_df = df.limit(number_of_rows)
+    return df.union(duplicate_df)
+
+def introduce_invalid_values(df, column_name, condition, invalid_value):
+
+    return df.withColumn(
+        column_name,
+        when(condition, lit(invalid_value))
+        .otherwise(col(column_name))
+    )
+
+dirty_config = [
+        {
+            "type": "null",
+            "column": "customer_city",
+            "condition": col("customer_zip_code_prefix") < 10005
+        },
+        {
+            "type": "duplicate",
+            "number_of_rows": 10
+        },
+        {
+            "type": "invalid",
+            "column": "customer_state",
+            "condition": col("customer_zip_code_prefix") > 99990,
+            "invalid_value": "XYZ"
+        }]
+
+
+def create_dirty_dataset(df, dirty_config):
+
+    for config in dirty_config:
+
+        if config["type"] == "null":
+
+            df = introduce_nulls(
+                df,
+                config["column"],
+                config["condition"]
+            )
+
+        elif config["type"] == "duplicate":
+
+            df = introduce_duplicates(
+                df,
+                config["number_of_rows"]
+            )
+
+        elif config["type"] == "invalid":
+
+            df = introduce_invalid_values(
+                df,
+                config["column"],
+                config["condition"],
+                config["invalid_value"]
+            )
+
+    return df
